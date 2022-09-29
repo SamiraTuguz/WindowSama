c2caaea5d7791e60340a9785b16e48d18fd6fbef
NataliaPopova127 <pnatala127@mail.ru>
2022-09-29 13:42:36 +0300

    add sln file

---
+++ Core/UserValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowAuthorization.Core
+{
+    internal class UserValidator
+    {
+        public static bool Validate(string login, string password)
+        {
+            if(login == "sama" && password == "samasama")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ View/AuthorizationWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WindowAuthorization.Core;
+
+namespace WindowAuthorization.View
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (UserValidator.Validate(tbLogin.Text, tbPassword.Text))
+            {
+                new DashboardWindow().Show();
+                Close();
+            }
+            else MessageBox.Show("Неверный логин или пароль");
+        }
+    }
+}
